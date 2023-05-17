acf78c42a1ba2f75bc88dd431eccad2dba000861
amir-njr <amirr.najjar.78@gmail.com>
2023-05-17 19:20:24 +0330

    Craete Login Page

--- src/components/Login/RightL.js
+++ src/components/Login/RightL.js
@@ -1,7 +1,31 @@
-export default function RightL () {
-    return (
-        <div className="bg-blue-400 h-full">
-            k
+// Icon
+import User from "../icon/User";
+// Atom
+import { Setting } from "../../../lib/atom";
+import { useAtom } from "jotai";
+// Component
+import SignIn from "./Signin";
+import SignUp from "./Signup";
+
+export default function RightL() {
+  const [setting, setSetting] = useAtom(Setting);
+  return (
+    <div className="bgLogin h-full flex justify-center items-center">
+      <div className="border w-[50%] bg-white flex flex-col gap-5 items-center p-4 relative">
+        <div className="flex flex-col gap-2 items-center">
+          <span className=" rounded-full h-20 w-20 bg-blue-300 flex justify-center items-center">
+            <User />
+          </span>
+          <h1>ورود به پنل</h1>
         </div>
-    )
+        <div className={`${setting.loginToggle ? "opacity-0" : "opacity-100"} absolute transition-all`}>
+            <SignUp />
+        </div>
+        <div className={`${setting.loginToggle ? "opacity-100" : "opacity-0"} absolute transition-all`}>
+            <SignIn />
+        </div>
+
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Login/Signup.js
@@ -0,0 +1,60 @@
+// Link
+import Link from "next/link";
+// Atom
+import { Setting } from "../../../lib/atom";
+import { useAtom } from "jotai";
+
+export default function SignUp() {
+  const [setting, setSetting] = useAtom(Setting);
+
+  return (
+    <form className="flex flex-col gap-10 shadow-xl bg-red-500 w-full p-2 rounded-md">
+      <input
+        className="border-r-4 border-r-blue-600 focus:outline-none focus:bg-slate-200 bg-slate-100 p-2"
+        placeholder="نام کاربری را وارد نمایید..."
+        type="text"
+        name="text"
+      />
+      <input
+        className="border-r-4 border-r-blue-600 focus:outline-none focus:bg-slate-200 bg-slate-100 p-2"
+        placeholder="رمز عبورتان را وارد نمایید..."
+        type="password"
+        name="password"
+      />
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            name="check"
+            id="check"
+            className="h-4 w-4 bg-ghaleb-orange"
+          />
+          <label for="check" className="text-sm">
+            مرا بخاطر بسپار!
+          </label>
+        </div>
+        <div>
+          <Link href="#" className="text-sm text-blue-600">
+            رمزتان را فراموش کرده اید؟
+          </Link>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <Link
+          className="bg-lime-500 w-28 text-center rounded-md py-2 hover:bg-lime-600 transition-all"
+          href="#"
+        >
+          ارسال
+        </Link>
+        <button
+          onClick={() =>
+            setSetting({ ...setting, loginToggle: !setting.loginToggle })
+          }
+          className="bg-blue-600 w-28 text-center rounded-md py-2 hover:bg-blue-700 transition-all"
+        >
+          ورود
+        </button>
+      </div>
+    </form>
+  );
+}
