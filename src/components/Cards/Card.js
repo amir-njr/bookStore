8d891bddf886027b2da9338176d92338e6b4aa9a
Behzad Khodapanah <u.wanna.track.ninja@gmail.com>
2023-05-29 13:42:17 +0330

    Update Index

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,79 +1,43 @@
+import Link from "next/link";
 import { useAtom } from "jotai";
 import { Setting } from "../../../lib/atom";
-import Link from "next/link";
-export default function Card(props) {
-  const [setting, seSetting] = useAtom(Setting);
-  const { title, price, image , id } = props;
-  const name = title.split(" ");
+// Icon
+import Dollar from "../icon/Dollar";
 
-  return (
-    <div className="border p-2 text-left shadow-xl">
-      <div>
-        <img src={image} />
-      </div>
-      <div className="flex flex-col gap-2">
-        <h1 className="text-2xl">{name[0]}</h1>
-        <div className="flex justify-between items-center">
-          <div>
-            {setting.counter == 0 && (
-              <button
-                onClick={() =>
-                  seSetting({ ...setting, counter: setting.counter + 1 })
-                }
-                className="bg-blue-600 p-2 rounded-xl"
-              >
-                سبد خرید
-              </button>
-            )}
+export default function Card({ bookData }) {
+	const [setting, seSetting] = useAtom(Setting);
+	console.log(bookData);
+	const { title, price, image, id } = bookData;
+	const name = title.split(" ");
 
-            {setting.counter > 0 && (
-              <div className="flex items-center">
-                <button
-                  onClick={() =>
-                    seSetting({ ...setting, counter: setting.counter + 1 })
-                  }
-                  className="bg-blue-600 p-2 rounded-xl"
-                >
-                  +
-                </button>
-                <button
-                  onClick={() =>
-                    seSetting({ ...setting, counter: (setting.counter = 0) })
-                  }
-                  className="bg-blue-600 p-2 rounded-xl"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </button>
+	return (
+		<div className="border p-2 text-left shadow-xl">
+			<div>
+				<img src={image} />
+			</div>
+			<div className="flex flex-col gap-10">
+				<h1 className="text-2xl">{name[0]}</h1>
+				<div className="flex justify-between items-center">
+					<div>
+						<button
+							onClick={() =>
+								seSetting({ ...setting, counter: setting.counter + 1 })
+							}
+							className="bg-blue-600 p-2 rounded-md text-white"
+						>
+							سبد خرید
+						</button>
+					</div>
 
-                <button
-                  onClick={() =>
-                    seSetting({ ...setting, counter: setting.counter - 1 })
-                  }
-                  className="bg-blue-600 p-2 rounded-xl"
-                >
-                  -
-                </button>
-              </div>
-            )}
-          </div>
-
-          <span className="bg-orange-400 p-2 rounded-xl">{price}</span>
-        </div>
-        <Link href={`/${id}`}>دیدن جزئیات</Link>
-      </div>
-    </div>
-  );
+					<span className="bg-orange-400 p-2 rounded-md flex items-center">{price}<Dollar /></span>
+				</div>
+				<Link
+					className="w-full text-center bg-purple-600 text-white py-2 rounded-md"
+					href={`/${id}`}
+				>
+					دیدن جزئیات
+				</Link>
+			</div>
+		</div>
+	);
 }
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
+import bookData from "../shared/bookData/bookData";
 import Card from "./Card";
 
-export default function Cards({books}) {
-  console.log(books)
+export default function Cards() {
+	const [data, setData] = useState([]);
 
-  return (
-    <div className="container mx-auto grid grid-cols-4 gap-4 my-10">
-      {/* {data.map((book) => (
-        <Card key={book.id} {...book} />
-      ))} */}
-    </div>
-  );
+	const loadData = async () => {
+		const res = await fetch("http://localhost:3000/api/books");
+		const data = await res.json();
+		return data;
+	};
+
+	useEffect(() => {
+		loadData().then((res) => {
+			setData(res);
+		});
+	}, []);
+	return (
+		<div className="container mx-auto grid grid-cols-4 gap-4 my-20">
+			{data.map((book) => (
+				<Card key={book.id} bookData={book} />
+			))}
+		</div>
+	);
 }
