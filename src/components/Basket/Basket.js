8c812517bb3909542520cdda68769ced1c77067e
amir-njr <amirr.najjar.78@gmail.com>
2023-06-20 12:58:58 +0330

    Update Basket Page

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -1,24 +1,55 @@
-<<<<<<< HEAD
-export default function Basket({ selected }) {
-  const { title, price, image, id, count , subtitle } = selected;
-  const splited = title.split(" ");
-  
-  return (
-    <div>
-      <div>
-        <img src={image} alt={title}/>
+import Check from "../icon/Check";
+import Dollar from "../icon/Dollar";
+import RecycleBin from "../icon/RecycleBin";
 
+export default function Basket({ selected }) {
+  const { title, price, image, id, count, subtitle } = selected;
+  const splited = title.split(" ");
+  console.log(selected);
+
+  return (
+    <div className="flex justify-between border-b-2 border-ghaleb-blue">
+      <div className="flex items-center">
+        <img src={image} alt={title} />
+        <div className="flex flex-col gap-10">
+          <h1 className="flex gap-2">
+            <span>نام کتاب:</span>
+            <span className="text-ghaleb-blue">{title}</span>
+          </h1>
+
+          <span className="flex items-center gap-2">
+            <span className="flex items-center">
+              <Dollar />
+              قیمت:
+            </span>
+            <span className="text-lime-500">{price}</span>
+          </span>
+          <span className="flex items-center gap-2">
+            <span>وضعیت:</span> <Check /> موجود
+          </span>
+          <button className="text-center rounded-md bg-purple-600 text-white px-4 py-2">
+            افزودن به سبد خرید
+          </button>
         </div>
-      <div></div>
+      </div>
+      <div className="flex items-center">
+        <span className="rounded-md text-center leading-10  h-10 w-10 font-bold text-white bg-orange-500">
+          {count}
+        </span>
+      </div>
+      <div className="flex items-center">
+        <div className="flex">
+          <button className="rounded-r-md h-10 w-10 font-bold text-white bg-blue-500 hover:bg-blue-600">
+            +
+          </button>
+          <button className="h-10 w-10 flex justify-center items-center text-white bg-red-500 hover:bg-red-600">
+            <RecycleBin />
+          </button>
+          <button className="rounded-l-md h-10 w-10 font-bold text-white bg-blue-500 hover:bg-blue-600">
+            -
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-=======
-export default function Basket () {
-  return (
-    <h1>
-      Basket
-    </h1>
-  )
-}
->>>>>>> 1255d3163a987d80ce80e47b557612007f6940d8
--- src/components/Basket/Baskets.js
+++ src/components/Basket/Baskets.js
@@ -3,18 +3,34 @@ import { BasketAtom } from "../../../lib/atom";
 import { useAtom } from "jotai";
 // Component
 import Basket from "./Basket";
+import Link from "next/link";
+
 export default function Baskets() {
   const [basket] = useAtom(BasketAtom);
   const { selectedItems } = basket;
 
-  console.log("b", selectedItems);
-
   return (
-    <section>
-      {
-        selectedItems.map(item => <Basket key={item.key} selected={item} />)
-      }
-      
+    <section className="flex my-10 gap-10">
+      <div className="basis-10/12">
+        {selectedItems.map((item) => (
+          <Basket key={item.key} selected={item} />
+        ))}
+      </div>
+      <div className="basis-2/12 border-2 flex flex-col justify-center px-1.5 gap-4 border-purple-500 rounded h-60">
+        <div className="flex justify-between">
+          <span className="text-ghaleb-blue">کل محصولات:</span>
+          <span>4</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-ghaleb-blue">قیمت کل:</span>
+          <span>5000</span>
+        </div>
+        <div className="flex justify-between">
+          <Link className="bg-lime-500 text-white px-3 rounded hover:bg-lime-600" href="">پرداخت</Link>
+
+          <button className="text-red-500">خالی کردن سبد</button>
+        </div>
+      </div>
     </section>
   );
 }
